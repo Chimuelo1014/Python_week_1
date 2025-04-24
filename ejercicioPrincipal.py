273b29bad97ed5cc915328dd415f4925d1def235
Chimuelo1014 <quinterosanchezsamuel1@gmail.com>
2025-04-24 17:27:13 -0500

    Update ejercicioPrincipal.py

--- ejercicioPrincipal.py
+++ ejercicioPrincipal.py
@@ -15,16 +15,16 @@ while True :
  
 while True :
     amount = int(input("Ingrese la cantidad de unidades del producto: "))
-    if amount >= 0 :
+    if amount >= 0 :                                                          ###Valida que no sea menor a cero y se ingrese una cantidad valida###
         break
     else :
-        print("¡Ingrese un valor valido!")   
+        print("¡Ingrese un valor valido!")                               ###si esta mal sale un aviso###
         
     
 porcen = input("¿Tiene descuento? Y(si)/N(no): ")
-if porcen == "Y" or porcen == "y":
+if porcen == "Y" or porcen == "y":                                         ###sirve para saber si se hara con descuento o no, si es asi, aplicara el descuento###
     
-    while True :
+    while True :                                                             
         discount = int(input("Ingrese el descuento a aplicar: "))
         if discount > 0 or discount < 100 :
             break
@@ -35,12 +35,12 @@ if porcen == "Y" or porcen == "y":
     descu = valor1*desc
     valortotal = valor1-descu 
 
-else :
+else :                                                                       ###si no aplicara descuento se ira por este lado###
     valor1 = unity*amount
     valortotal = valor1
    
  
-print("Producto: ",name)
+print("Producto: ",name)                                  ###se imprimen el nombre del producto , el precio de unidad y el valor total con descuento o no, segun el caso###
 print("Precio unitario: $","%.2f" % unity)
 print("Valor total: $","%.2f" % valortotal)
 
